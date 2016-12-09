43fd4b7ad5350bc6bdc9bd7a5c85bd096fd86979
Jaseem Abid <jaseemabid@gmail.com>
2016-12-09 14:58:36 +0530

    Add some sample python code

--- py/main.py
+++ py/main.py
@@ -1,8 +1,40 @@
 """ Scratch Workspace """
 
+from functools import wraps
+
+def accepts(*types):
+    def check_accepts(f):
+        assert len(types) == f.func_code.co_argcount
+        def new_f(*args, **kwds):
+            for (a, t) in zip(args, types):
+                assert isinstance(a, t), \
+                       "arg %r does not match %s" % (a,t)
+            return f(*args, **kwds)
+        new_f.func_name = f.func_name
+        return new_f
+    return check_accepts
+
+
+def returns(rtype):
+    def check_returns(f):
+        def new_f(*args, **kwds):
+            result = f(*args, **kwds)
+            assert isinstance(result, rtype), \
+                   "return value %r does not match %s" % (result,rtype)
+            return result
+        new_f.func_name = f.func_name
+        return new_f
+    return check_returns
+
+
+@accepts(int)
+@returns(int)
+def square(x):
+    return x * x
+
 def main():
     print "hello world"
-
+    print square(4)
 
 if __name__ == '__main__':
     main()
