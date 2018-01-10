05e2570c30cb9050f79c65da0fa3f9d29fe421c1
Jaseem Abid <jaseemabid@gmail.com>
2018-01-10 08:39:47 +0530

    Add some type level examples

--- hs/src/Main.hs
+++ hs/src/Main.hs
@@ -1,4 +1,26 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+
 module Main where
 
 main :: IO ()
 main = putStrLn "hello world"
+
+
+data Power = Petrol | Pedal
+
+data Vehicle (a :: Power) where
+    Bike  :: Vehicle Pedal
+    Car   :: Int -> Vehicle Petrol
+    Bus   :: Int -> Vehicle Petrol
+    Tesla :: Vehicle a
+
+go :: Vehicle a -> String
+go Bike = "Tring tring"
+go (Car fuel) = "Vroom"
+go (Bus fuel) = "Bom"
+
+refuel :: Int -> Vehicle Petrol -> Vehicle Petrol
+refuel k (Car fuel) = Car $ k + fuel
+refuel k (Bus fuel) = Car $ k + fuel
