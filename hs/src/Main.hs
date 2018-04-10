e0db3ac4fdc7d65d787857125333c9e0240cc740
Jaseem Abid <jaseemabid@gmail.com>
2018-04-10 17:51:00 +0530

    Fix GHC warnings

--- hs/src/Main.hs
+++ hs/src/Main.hs
@@ -11,16 +11,18 @@ main = putStrLn "hello world"
 data Power = Petrol | Pedal
 
 data Vehicle (a :: Power) where
-    Bike  :: Vehicle Pedal
-    Car   :: Int -> Vehicle Petrol
-    Bus   :: Int -> Vehicle Petrol
+    Bike  :: Vehicle 'Pedal
+    Car   :: Int -> Vehicle 'Petrol
+    Bus   :: Int -> Vehicle 'Petrol
     Tesla :: Vehicle a
 
 go :: Vehicle a -> String
-go Bike = "Tring tring"
-go (Car fuel) = "Vroom"
-go (Bus fuel) = "Bom"
+go Bike    = "Tring tring"
+go (Car _) = "Vroom"
+go (Bus _) = "Bom"
+go Tesla   = "Eee"
 
-refuel :: Int -> Vehicle Petrol -> Vehicle Petrol
+refuel :: Int -> Vehicle 'Petrol -> Vehicle 'Petrol
 refuel k (Car fuel) = Car $ k + fuel
 refuel k (Bus fuel) = Car $ k + fuel
+refuel _ Tesla      = Tesla
